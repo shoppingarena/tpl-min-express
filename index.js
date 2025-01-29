7d3e0646719e657e556255b845689fd0d6c86735
Jiri Benes <benes.jiri@shoppingarena.eu>
2025-01-29 17:13:17 +0200

    +install Consolidate to use React template in Express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
+import 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
