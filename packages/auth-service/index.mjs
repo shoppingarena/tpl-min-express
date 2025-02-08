f70addd197669f40273a681913091258888ebbda
Jiri Benes <benes.jiri@shoppingarena.eu>
2025-02-08 15:50:18 +0200

    add tailwindcss

--- packages/auth-service/index.mjs
+++ packages/auth-service/index.mjs
@@ -9,6 +9,7 @@ import path, { dirname } from 'node:path';
 // import chalk from 'chalk' => postRouter.mjs
 import getRoute from './server/getRouter.mjs'
 import postRoute from './server/postRouter.mjs'
+import tailwindcss from 'tailwindcss'
 
 
 
